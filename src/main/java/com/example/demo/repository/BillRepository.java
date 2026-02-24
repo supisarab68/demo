32e342ce54abacd5e332e3de010065c6b0345395
poom79186-ctrl <poom79186@gmail.com>
2026-02-24 16:10:43 +0700

    up date code ฝากเขียน code อาหารด้วย

--- src/main/java/com/example/demo/repository/BillRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BillRepository extends JpaRepository<Bill, Long> {
-}
