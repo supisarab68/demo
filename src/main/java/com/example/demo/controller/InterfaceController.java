f49813376b9070ed2eaf196e1ad0086dd9f6c77a
saifaw68-alt <saifaw68@nu.ac.th>
2026-03-03 14:47:30 +0700

    addimagmenu

--- src/main/java/com/example/demo/controller/InterfaceController.java
+++ src/main/java/com/example/demo/controller/InterfaceController.java
@@ -2,13 +2,18 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class InterfaceController {
-    @GetMapping("/interface")
+
+    @GetMapping("/")
     public String home() {
-        return "index";  
+        return "index";
+    }
 
-}
-
+    @PostMapping("/order")
+    public String order() {
+        return "orderStatus";
+    }
 }
